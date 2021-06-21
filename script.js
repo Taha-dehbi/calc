f5545fa356cdeb4acb2a66d08faef21964163b03
Taha-dehbi <78152651+Taha-dehbi@users.noreply.github.com>
2021-06-21 12:30:13 +0100

    Update script.js

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ function msg(){
     document.getElementById('display').innerText=n1+n2;
 }
 function msg2(){
-    var n1=document.getElementById('n1').value=0;
-    var n2=document.getElementById('n2').value=0;
+    var n1=document.getElementById('n1').value="";
+    var n2=document.getElementById('n2').value="";
     document.getElementById('display').innerText=0;
 }
 function msg3(){
